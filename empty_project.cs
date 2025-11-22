437f8d5d69e267be675659b5c30812d854d8960b
mthierman <mthierman@gmail.com>
2025-11-22 12:19:17 -0700

    empty_project

---
+++ empty_project.cs
@@ -0,0 +1,11 @@
+﻿#:package Microsoft.Build@18.0.2
+using Microsoft.Build.Construction;
+
+var project = ProjectRootElement.Create();
+project.Sdk = "Microsoft.NET.Sdk";
+project.AddProperty("OutputType", "Exe");
+project.AddProperty("TargetFramework", "net7.0");
+var itemGroup = project.AddItemGroup();
+itemGroup.AddItem("Compile", "Program.cs");
+project.Save("out/app.vcxproj");
+Console.WriteLine("Project file generated!");
---
+++ empty_solution.cs
@@ -0,0 +1,11 @@
+#:package Microsoft.VisualStudio.SolutionPersistence@1.0.52
+using Microsoft.VisualStudio.SolutionPersistence.Model;
+using Microsoft.VisualStudio.SolutionPersistence.Serializer;
+
+var solution = new SolutionModel();
+
+solution.AddProject("app.vcxproj");
+solution.AddPlatform("x64");
+solution.AddPlatform("x86");
+
+await SolutionSerializers.SlnXml.SaveAsync("out/app.slnx", solution, new CancellationToken());
