aa79e8f1e0c6b064c13853379a5752d3b6fcf913
mthierman <mthierman@gmail.com>
2025-11-22 17:38:52 -0700

    Add working fmt from vcpkg to example

--- example/empty_project/main.cpp
+++ example/empty_project/main.cpp
@@ -1,9 +1,12 @@
+#include <fmt/core.h>
 import std;
 import my_module;
 
 // https://learn.microsoft.com/en-us/cpp/c-language/using-wmain
 auto wmain([[maybe_unused]] int argc, [[maybe_unused]] wchar_t* argv[], [[maybe_unused]] wchar_t* envp[]) -> int {
     std::println();
+    fmt::print("Hello World!\n");
+
     my_func();
 
     return 0;
