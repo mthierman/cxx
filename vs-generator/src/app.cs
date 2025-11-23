514e5cf8e36e547740dc6deb5de7d8bca9bd071d
mthierman <mthierman@gmail.com>
2025-11-23 13:30:47 -0700

    Fix run()

--- vs-generator/src/app.cs
+++ vs-generator/src/app.cs
@@ -1,22 +1,56 @@
+using System.CommandLine;
 using System.Reflection;
 
-public partial class App
+public class App
 {
-    public static string src_dir { get; } = Path.Combine(Environment.CurrentDirectory, "src");
-    public static string build_dir { get; } = Path.Combine(Environment.CurrentDirectory, "build");
-    public static string version { get; } = Assembly.GetExecutingAssembly()
-                  .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
-                  .InformationalVersion ?? string.Empty;
-
     public enum ExitCode : int
     {
         Success = 0,
         GeneralError = 1,
     }
+    public static string src_dir { get; } = Path.Combine(Environment.CurrentDirectory, "src");
+    public static string build_dir { get; } = Path.Combine(Environment.CurrentDirectory, "build");
+    public static string version { get; } = Assembly.GetExecutingAssembly()
+                  .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
+                  .InformationalVersion ?? string.Empty;
+    private static RootCommand root_command { get; } = new RootCommand($"vs-generator {version}");
+    private static Dictionary<string, Command> sub_command = new Dictionary<string, Command>
+    {
+        ["gen"] = new Command("gen", "Generate build"),
+        ["build"] = new Command("build", "Build debug"),
+        ["release"] = new Command("release", "Build release"),
+        ["clean"] = new Command("clean", "Clean build")
+    };
 
     public int run(string[] args)
     {
-        App.parse_args(args);
+        foreach (var command in sub_command.Values)
+        {
+            root_command.Subcommands.Add(command);
+        }
+
+        sub_command["gen"].SetAction(async parseResult =>
+        {
+            return (await MSBuild.generate()) ? 0 : 1;
+        });
+
+
+        sub_command["build"].SetAction(async parseResult =>
+        {
+            return (await MSBuild.generate() && MSBuild.build(MSBuild.BuildConfiguration.Debug)) ? 0 : 1;
+        });
+
+        sub_command["release"].SetAction(async parseResult =>
+        {
+            return (await MSBuild.generate() && MSBuild.build(MSBuild.BuildConfiguration.Release)) ? 0 : 1;
+        });
+
+        sub_command["clean"].SetAction(async parseResult =>
+        {
+            Directory.Delete(build_dir, true);
+            return 0;
+        });
+
         return root_command.Parse(args).Invoke();
     }
 }
