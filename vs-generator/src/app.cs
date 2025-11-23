c3c060011f2f4a1ad8bc8ac1a4f080543f856e06
mthierman <mthierman@gmail.com>
2025-11-23 13:24:09 -0700

    Partial App class

--- vs-generator/src/app.cs
+++ vs-generator/src/app.cs
@@ -1,7 +1,7 @@
 using System.CommandLine;
 using System.Reflection;
 
-public class App
+public partial class App
 {
     public enum ExitCode : int
     {
@@ -13,44 +13,9 @@ public class App
     public static string version { get; } = Assembly.GetExecutingAssembly()
                   .GetCustomAttribute<AssemblyInformationalVersionAttribute>()?
                   .InformationalVersion ?? string.Empty;
-    private RootCommand root_command { get; } = new RootCommand($"vs-generator {version}");
-    private Dictionary<string, Command> commands = new Dictionary<string, Command>
-    {
-        ["gen"] = new Command("gen", "Generate build"),
-        ["build"] = new Command("build", "Build debug"),
-        ["release"] = new Command("release", "Build release"),
-        ["clean"] = new Command("clean", "Clean build")
-    };
 
     public int run(string[] args)
     {
-        foreach (var command in commands.Values)
-        {
-            root_command.Subcommands.Add(command);
-        }
-
-        commands["gen"].SetAction(async parseResult =>
-        {
-            return (await MSBuild.generate()) ? 0 : 1;
-        });
-
-
-        commands["build"].SetAction(async parseResult =>
-        {
-            return (await MSBuild.generate() && MSBuild.build(MSBuild.BuildConfiguration.Debug)) ? 0 : 1;
-        });
-
-        commands["release"].SetAction(async parseResult =>
-        {
-            return (await MSBuild.generate() && MSBuild.build(MSBuild.BuildConfiguration.Release)) ? 0 : 1;
-        });
-
-        commands["clean"].SetAction(async parseResult =>
-        {
-            Directory.Delete(build_dir, true);
-            return 0;
-        });
-
         return root_command.Parse(args).Invoke();
     }
 }
---
+++ vs-generator/src/command.cs
@@ -0,0 +1,45 @@
+using System.CommandLine;
+
+public partial class App
+{
+    private RootCommand root_command { get; } = new RootCommand($"vs-generator {version}");
+    private Dictionary<string, Command> commands = new Dictionary<string, Command>
+    {
+        ["gen"] = new Command("gen", "Generate build"),
+        ["build"] = new Command("build", "Build debug"),
+        ["release"] = new Command("release", "Build release"),
+        ["clean"] = new Command("clean", "Clean build")
+    };
+
+    public int setup_commands(string[] args)
+    {
+        foreach (var command in commands.Values)
+        {
+            root_command.Subcommands.Add(command);
+        }
+
+        commands["gen"].SetAction(async parseResult =>
+        {
+            return (await MSBuild.generate()) ? 0 : 1;
+        });
+
+
+        commands["build"].SetAction(async parseResult =>
+        {
+            return (await MSBuild.generate() && MSBuild.build(MSBuild.BuildConfiguration.Debug)) ? 0 : 1;
+        });
+
+        commands["release"].SetAction(async parseResult =>
+        {
+            return (await MSBuild.generate() && MSBuild.build(MSBuild.BuildConfiguration.Release)) ? 0 : 1;
+        });
+
+        commands["clean"].SetAction(async parseResult =>
+        {
+            Directory.Delete(build_dir, true);
+            return 0;
+        });
+
+        return root_command.Parse(args).Invoke();
+    }
+}
