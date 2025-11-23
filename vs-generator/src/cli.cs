1b680c8f2363b45a4786536c56afd7878c57bb60
mthierman <mthierman@gmail.com>
2025-11-23 01:28:50 -0700

    Fix args

--- vs-generator/src/cli.cs
+++ vs-generator/src/cli.cs
@@ -1,17 +1,15 @@
 using System.CommandLine;
 using System.Diagnostics;
-// using Microsoft.Build.Evaluation;
 
 public class App
 {
-    public static int run(string[]? args)
+    public static int run(string[] args)
     {
         RootCommand root_command = new("vs-generator");
         Console.WriteLine(root_command.Description);
 
         Command gen = new("gen", "Generate build files") { };
         root_command.Subcommands.Add(gen);
-
         gen.SetAction(async parseResult =>
         {
             await MSBuild.generate_project();
@@ -19,21 +17,17 @@ public class App
 
         Command build = new("build", "Build project") { };
         root_command.Subcommands.Add(build);
-
         build.SetAction(async parseResult =>
         {
             var build_dir = Path.Combine(Environment.CurrentDirectory, "build");
+
             if (Directory.Exists(build_dir))
             {
-                var start_info = new ProcessStartInfo() { FileName = "msbuild", WorkingDirectory = build_dir };
-                using var process = Process.Start(start_info);
+                using var process = Process.Start(new ProcessStartInfo() { FileName = "msbuild", WorkingDirectory = build_dir });
                 process?.WaitForExit();
-                // var pc = new ProjectCollection();
-                // var project = pc.LoadProject(Path.Combine(build_dir, "app.vcxproj"));
-                // project.Build();
             }
         });
 
-        return root_command.Parse(args!).Invoke();
+        return root_command.Parse(args).Invoke();
     }
 }
