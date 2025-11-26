d5355e958b4b9a83d97ed393ebc7f1551605ea23
mthierman <mthierman@gmail.com>
2025-11-25 21:33:45 -0700

    Move Find functions

--- src/Paths.cs
+++ src/Paths.cs
@@ -87,148 +87,58 @@ public static partial class App
         return new EnvironmentPaths(corePaths, toolsPaths);
     }
 
-    //     public static async Task<int> RunProcess(string? command, string[] args)
-    //     {
-    //         var startInfo = new ProcessStartInfo
-    //         {
-    //             FileName = command,
-    //             Arguments = string.Join(" ", args),
-    //             RedirectStandardOutput = true,
-    //             RedirectStandardError = true,
-    //             UseShellExecute = false
-    //         };
 
-    //         using var process = new Process { StartInfo = startInfo };
-    //         process.OutputDataReceived += (s, e) => { if (e.Data != null) Console.WriteLine(e.Data); };
-    //         process.ErrorDataReceived += (s, e) => { if (e.Data != null) Console.Error.WriteLine(e.Data); };
+    private static string? FindOnPath(string command)
+    {
+        var pathEnv = Environment.GetEnvironmentVariable("PATH");
 
-    //         process.Start();
-    //         process.BeginOutputReadLine();
-    //         process.BeginErrorReadLine();
+        if (string.IsNullOrEmpty(pathEnv))
+            return null;
 
-    //         await process.WaitForExitAsync();
+        string[] paths = pathEnv.Split(Path.PathSeparator);
 
-    //         return 0;
-    //     }
+        foreach (var dir in paths)
+        {
+            string fullPath = Path.Combine(dir, command);
+            if (File.Exists(fullPath))
+                return fullPath;
+        }
 
-    //     public static async Task<int> NewProject()
-    //     {
-    //         if (Directory.EnumerateFileSystemEntries(Environment.CurrentDirectory).Any())
-    //         {
-    //             Console.ForegroundColor = ConsoleColor.Red;
-    //             Console.Error.WriteLine("Directory is not empty");
-    //             Console.ResetColor();
+        return null;
+    }
 
-    //             return 1;
-    //         }
+    private static string? FindRepoRoot()
+    {
+        for (var cwd = Environment.CurrentDirectory;
+            !string.IsNullOrEmpty(cwd);
+            cwd = Directory.GetParent(cwd)?.FullName)
+        {
+            if (File.Exists(Path.Combine(cwd, ManifestFile)))
+                return cwd;
+        }
 
-    //         var cwd = Environment.CurrentDirectory;
-    //         var blankManifest = Path.Combine(cwd, "cxx.jsonc");
-    //         var vcpkgManifest = Path.Combine(cwd, "vcpkg.json");
-    //         var vcpkgConfig = Path.Combine(cwd, "vcpkg-configuration.json");
+        return null;
+    }
 
-    //         if (File.Exists(ManifestFile) || File.Exists(vcpkgManifest) || File.Exists(vcpkgConfig))
-    //         {
-    //             Console.ForegroundColor = ConsoleColor.Red;
-    //             Console.Error.WriteLine("Project already has a manifest file");
-    //             Console.ResetColor();
+    private static string? FindMSBuild(string vswhere)
+    {
+        using var process = Process.Start(new ProcessStartInfo(vswhere,
+            "-latest -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\amd64\\MSBuild.exe")
+        {
+            RedirectStandardOutput = true
+        });
 
-    //             return 1;
-    //         }
+        if (process is null)
+            return null;
 
-    //         await File.WriteAllTextAsync(blankManifest, "{}");
+        var output = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
 
-    //         RunVcpkg("new", "--application");
+        var found = output
+            .Split('\r', '\n', StringSplitOptions.RemoveEmptyEntries)
+            .Select(s => s.Trim())
+            .FirstOrDefault();
 
-    //         if (!Directory.Exists(Paths.Core.Src))
-    //             Directory.CreateDirectory(Paths.Core.Src);
-
-    //         var app_cpp = Path.Combine(Paths.Core.Src, "app.cpp");
-
-    //         if (!File.Exists(app_cpp))
-    //         {
-    //             await File.WriteAllTextAsync(app_cpp, @"
-    // #include <print>
-
-    // auto wmain() -> int {
-    //     std::println(""Hello, World!"");
-
-    //     return 0;
-    // }
-    // ".Trim());
-    //         }
-
-    //         return 0;
-    //     }
-
-    //     public static int RunVcpkg(params string[] arguments)
-    //     {
-    //         var startInfo = new ProcessStartInfo("vcpkg");
-
-    //         foreach (var argument in arguments)
-    //         {
-    //             startInfo.ArgumentList.Add(argument);
-    //         }
-
-    //         startInfo.EnvironmentVariables["VCPKG_DEFAULT_TRIPLET"] = "x64-windows-static-md";
-    //         startInfo.EnvironmentVariables["VCPKG_DEFAULT_HOST_TRIPLET"] = "x64-windows-static-md";
-
-    //         Process.Start(startInfo)?.WaitForExit();
-
-    //         return 0;
-    //     }
-
-    //     private static string? FindOnPath(string command)
-    //     {
-    //         var pathEnv = Environment.GetEnvironmentVariable("PATH");
-
-    //         if (string.IsNullOrEmpty(pathEnv))
-    //             return null;
-
-    //         string[] paths = pathEnv.Split(Path.PathSeparator);
-
-    //         foreach (var dir in paths)
-    //         {
-    //             string fullPath = Path.Combine(dir, command);
-    //             if (File.Exists(fullPath))
-    //                 return fullPath;
-    //         }
-
-    //         return null;
-    //     }
-
-    //     private static string? FindRepoRoot()
-    //     {
-    //         for (var cwd = Environment.CurrentDirectory;
-    //             !string.IsNullOrEmpty(cwd);
-    //             cwd = Directory.GetParent(cwd)?.FullName)
-    //         {
-    //             if (File.Exists(Path.Combine(cwd, ManifestFile)))
-    //                 return cwd;
-    //         }
-
-    //         return null;
-    //     }
-
-    //     private static string? FindMSBuild(string vswhere)
-    //     {
-    //         using var process = Process.Start(new ProcessStartInfo(vswhere,
-    //             "-latest -requires Microsoft.Component.MSBuild -find MSBuild\\**\\Bin\\amd64\\MSBuild.exe")
-    //         {
-    //             RedirectStandardOutput = true
-    //         });
-
-    //         if (process is null)
-    //             return null;
-
-    //         var output = process.StandardOutput.ReadToEnd();
-    //         process.WaitForExit();
-
-    //         var found = output
-    //             .Split('\r', '\n', StringSplitOptions.RemoveEmptyEntries)
-    //             .Select(s => s.Trim())
-    //             .FirstOrDefault();
-
-    //         return string.IsNullOrWhiteSpace(found) ? null : found;
-    //     }
+        return string.IsNullOrWhiteSpace(found) ? null : found;
+    }
 }
