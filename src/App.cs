7f2fb3ee3b89ce419e867219843e8b66a4e1b3dc
mthierman <mthierman@gmail.com>
2025-11-27 15:56:45 -0700

    Move CommandLine to App

--- src/App.cs
+++ src/App.cs
@@ -1,3 +1,5 @@
+using System.CommandLine;
+using System.Diagnostics;
 using System.Reflection;
 
 namespace cxx;
@@ -10,6 +12,115 @@ public static class App
 
     public static int Run(string[] args)
     {
-        return CommandLine.RootCommand.Parse(args).Invoke();
+        return RootCommand.Parse(args).Invoke();
+    }
+
+    private static readonly SemaphoreSlim ConsoleLock = new SemaphoreSlim(1, 1);
+    private static RootCommand RootCommand { get; } = new RootCommand($"C++ build tool\nversion {App.Version}");
+    private static Argument<MSBuild.BuildConfiguration> BuildConfiguration = new("BuildConfiguration") { Arity = ArgumentArity.ZeroOrOne, Description = "Build Configuration (debug or release). Default: debug" };
+    private static Argument<string[]> MSBuildArguments = new Argument<string[]>("Args") { Arity = ArgumentArity.ZeroOrMore };
+    private static Argument<string[]> NinjaArguments = new Argument<string[]>("Args") { Arity = ArgumentArity.ZeroOrMore };
+    private static Argument<string[]> VcpkgArguments = new Argument<string[]>("Args") { Arity = ArgumentArity.ZeroOrMore };
+    private static Dictionary<string, Command> SubCommand = new Dictionary<string, Command>
+    {
+        ["devenv"] = new Command("devenv", "Refresh developer environment"),
+        ["msbuild"] = new Command("msbuild") { MSBuildArguments },
+        ["ninja"] = new Command("ninja") { NinjaArguments },
+        ["vcpkg"] = new Command("vcpkg") { VcpkgArguments },
+        ["new"] = new Command("new", "New project"),
+        ["install"] = new Command("install", "Install project dependencies"),
+        ["generate"] = new Command("generate", "Generate project build"),
+        ["build"] = new Command("build", "Build project") { BuildConfiguration },
+        ["run"] = new Command("run", "Run project") { BuildConfiguration },
+        ["publish"] = new Command("publish", "Publish project"),
+        ["clean"] = new Command("clean", "Clean project"),
+        ["format"] = new Command("format", "Format project sources"),
+    };
+
+    static App()
+    {
+        foreach (var command in SubCommand.Values)
+        {
+            RootCommand.Subcommands.Add(command);
+        }
+
+        SubCommand["devenv"].SetAction(async parseResult =>
+        {
+            var devEnv = await MSBuild.DevEnv;
+
+            foreach (var kv in devEnv)
+            {
+                Console.WriteLine($"{kv.Key} = {kv.Value}");
+            }
+
+            Console.WriteLine();
+
+            var sdk = await MSBuild.GetWindowsSdkExecutablePath();
+            Console.WriteLine(sdk);
+
+            // await ExternalCommand.Run(await MSBuild.DevEnvironmentTools.MSBuild(), "-version");
+            // return await ExternalCommand.Run(await MSBuild.DevEnvironmentTools.RC(), "-version");
+        });
+
+        SubCommand["msbuild"].SetAction(async parseResult =>
+        {
+            return await MSBuild.Run(parseResult.GetValue(MSBuildArguments));
+        });
+
+        SubCommand["ninja"].SetAction(async parseResult =>
+        {
+            return await Ninja.Run(parseResult.GetValue(NinjaArguments));
+        });
+
+        SubCommand["vcpkg"].SetAction(async parseResult =>
+        {
+            return await Vcpkg.Run(parseResult.GetValue(VcpkgArguments));
+        });
+
+        SubCommand["new"].SetAction(async parseResult =>
+        {
+            return await Project.New();
+        });
+
+        SubCommand["install"].SetAction(async parseResult =>
+        {
+            return await ExternalCommand.RunVcpkg("install");
+        });
+
+        SubCommand["generate"].SetAction(async parseResult =>
+        {
+            return await MSBuild.Generate();
+        });
+
+        SubCommand["build"].SetAction(async parseResult =>
+        {
+            return await MSBuild.Build(parseResult.GetValue(BuildConfiguration));
+        });
+
+        SubCommand["run"].SetAction(async parseResult =>
+        {
+            await MSBuild.Build(parseResult.GetValue(BuildConfiguration));
+
+            Process.Start(new ProcessStartInfo(Path.Combine(Project.Core.Build, parseResult.GetValue(BuildConfiguration) == MSBuild.BuildConfiguration.Debug ? "debug" : "release", "app.exe")))?.WaitForExit();
+
+            return 0;
+        });
+
+        SubCommand["publish"].SetAction(async parseResult =>
+        {
+            return 0;
+        });
+
+        SubCommand["clean"].SetAction(async parseResult =>
+        {
+            return MSBuild.Clean();
+        });
+
+        SubCommand["format"].SetAction(async parseResult =>
+        {
+            await Clang.FormatAsync();
+
+            return 0;
+        });
     }
 }
